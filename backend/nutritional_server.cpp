e1bccf94a1a39f27e266e93b93e64ec4ed08bb8c
Diego <Dgomez.1730@hotmail.com>
2019-12-02 04:11:48 -0500

    Proper update of user table once the survey has been filled out

--- backend/nutritional_server.cpp
+++ backend/nutritional_server.cpp
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <string>
+#include <cmath>
 #include <iostream>
 #include <mysql.h>
 #include <mysqlx/xdevapi.h>
@@ -68,7 +70,7 @@ void NutritionalServer::handle_post(http_request message) {
 		else {
 			ucout << "User has an account and can log in" << endl;
 		}
-	}
+	} 
 	else if (strcmp(utility::conversions::to_utf8string(source).c_str(), "registration") == 0) {
 		utility::string_t emailField = utility::conversions::to_string_t("email");
 		utility::string_t fNameField = utility::conversions::to_string_t("fName");
@@ -87,14 +89,62 @@ void NutritionalServer::handle_post(http_request message) {
 		} 
 		catch (const Error & err) {
 			cout << "ERROR: " << err << endl;
-			message.reply(status_codes::NotAcceptable);
+			message.reply(status_codes::NotAcceptable); // May need to be modified to ensure for proper reply
 		}
+
+		// Set up the response of the user's information as a json 
 		if (table.count() == (count + 1)) {
-			ucout << "User has successfully regestered" << endl;
+			ucout << "User has successfully registered" << endl;
 		}
 		else {
 			ucout << "Username or Email is not unique" << endl;
 		}
+	}
+	else if (strcmp(utility::conversions::to_utf8string(source).c_str(), "survey") == 0) {
+		utility::string_t emailField = utility::conversions::to_string_t("email");
+		utility::string_t heightField = utility::conversions::to_string_t("height");
+		utility::string_t weightField = utility::conversions::to_string_t("weight");
+		utility::string_t genderField = utility::conversions::to_string_t("gender");
+		utility::string_t DOBField = utility::conversions::to_string_t("DOB");
+		utility::string_t activityField = utility::conversions::to_string_t("lvlOfActivity");
+		utility::string_t restrictField = utility::conversions::to_string_t("dietaryRestriction");
+
+		utility::string_t email = (message.headers().find(emailField))->second;
+		utility::string_t height = (message.headers().find(heightField))->second;
+		utility::string_t weight = (message.headers().find(weightField))->second;
+		utility::string_t gender = (message.headers().find(genderField))->second;
+		utility::string_t DOB = (message.headers().find(DOBField))->second;
+		utility::string_t activity = (message.headers().find(activityField))->second;
+		utility::string_t restrict = (message.headers().find(restrictField))->second;
+
+		int birthYear = stoi(convertToStd(DOB).substr(0,4));
+		int birthMonth = stoi(convertToStd(DOB).substr(5, 6));
+		int birthDay = stoi(convertToStd(DOB).substr(8, 9));
+		
+		int currentYear = stoi(convertToStd(datetime::utc_now().to_string()).substr(11, 15));
+
+		Session sess("localhost", 33060, "root", "root");
+		Schema db = sess.getSchema("user_db");
+		Table table = db.getTable("users");
+
+		try {
+			table.update()
+				.set("height", convertToStd(height))
+				.set("weight", convertToStd(weight))
+				.set("gender", convertToStd(gender))
+				.set("age", currentYear - birthYear)
+				.set("BMI", (703 * stoi(convertToStd(weight))) / pow(stoi(convertToStd(height)), 2))
+				.set("activity_level", convertToStd(activity))
+				.set("diet_restriction", convertToStd(restrict))
+				.where("username like :username").bind("username", convertToStd(username))
+				.execute();
+		}
+		catch (const Error & err) {
+			cout << "ERROR: " << err << endl;
+			message.reply(status_codes::NotAcceptable); // May need to be modified to ensure for proper reply
+		}
+
+		ucout << "Survey information has been updated" << endl;
 	}
 	message.reply(status_codes::OK);
 }
