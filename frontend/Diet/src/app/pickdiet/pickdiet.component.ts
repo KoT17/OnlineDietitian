6181dfa2cc4b98dd8413da3f4fde68eada999406
elenuhhh <berishaelena@gmail.com>
2019-12-03 23:53:17 -0500

    more json linking

--- frontend/Diet/src/app/pickdiet/pickdiet.component.ts
+++ frontend/Diet/src/app/pickdiet/pickdiet.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { SignInService } from '../sign-in.service';
+import { throwError } from 'rxjs';
+import { take, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pickdiet',
@@ -9,26 +11,52 @@ import { SignInService } from '../sign-in.service';
   styleUrls: ['./pickdiet.component.css']
 })
 export class PickdietComponent implements OnInit {
-  httpOptions = new HttpHeaders({
-    "user": "",
-    "password": "",
-    "source": "pick"
-  })
-
+  options;
   constructor(private signInService: SignInService, private http: HttpClient, private router: Router) {
-    this.httpOptions.set('user', signInService.getUser().email);
-    this.httpOptions.set('password', signInService.getUser().password);
+
   }
 
   ngOnInit() {
+    this.http.get('http://localhost:4201/NutritionalLookup', { headers: new HttpHeaders({
+      source: 'diet_choice',
+      user: this.signInService.getUser().email,
+      password: this.signInService.getUser().password
+    })}).pipe(take(1)).pipe(catchError(this.handleError)).subscribe(res => {
+      console.log(res)
+      this.options = res;
+    });
   }
 
   select(option) {
     if(option == 1) {
-      this.http.post("pickURL", { diet: ""}, { headers: this.httpOptions})
+      this.http.post("http://localhost:4201/NutritionalLookup", {} , { headers: new HttpHeaders({
+        source: "selection",
+        user: this.signInService.getUser().email,
+        password: this.signInService.getUser().password,
+        diet: this.options.firstDiet.DietName.toString()
+      })}).pipe(take(1)).pipe(catchError(this.handleErrorPost)).subscribe(res => {
+        this.router.navigateByUrl('/authhome');
+      });
     } else {
-      this.http.post("pickURL", { diet: ""}, { headers: this.httpOptions})
+      this.http.post("http://localhost:4201/NutritionalLookup", {} , { headers: new HttpHeaders({
+        source: "selection",
+        user: this.signInService.getUser().email,
+        email: this.signInService.getUser().email,
+        password: this.signInService.getUser().password,
+        diet: this.options.secondDiet.DietName.toString()
+      })}).pipe(take(1)).pipe(catchError(this.handleErrorPost)).subscribe(res => {
+        this.router.navigateByUrl('/authhome');
+      });
     }
-    this.router.navigateByUrl('/authhome');
   }
+
+  handleError(error: HttpErrorResponse) {
+     alert("Couldn't retrieve diet selections!");
+     return throwError('');
+   }
+
+   handleErrorPost(error: HttpErrorResponse) {
+     alert("Couldn't post diet selection!");
+     return throwError('');
+   }
 }
--- frontend/Diet/src/app/user-home/user-home.component.ts
+++ frontend/Diet/src/app/user-home/user-home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { SignInService } from '../sign-in.service';
+import { throwError } from 'rxjs';
+import { take, catchError } from 'rxjs/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-home',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-home.component.css']
 })
 export class UserHomeComponent implements OnInit {
+  diets = [1,2,3,4,5,6,7]
 
-  constructor() { }
+  constructor(private signInService: SignInService, private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get('http://localhost:4201/NutritionalLookup', { headers: new HttpHeaders({
+      source: 'user_info',
+      user: this.signInService.getUser().email,
+      password: this.signInService.getUser().password
+    })}).pipe(take(1)).pipe(catchError(this.handleError)).subscribe(res => {
+      console.log(res)
+    });
   }
 
+  handleError(error: HttpErrorResponse) {
+     alert("Couldn't retrieve diets!");
+     return throwError('');
+   }
 }
